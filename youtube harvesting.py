49952da06abc22aeffbe25f63d9d638c98580b7a
abhi-1009 <sinhaabhijit12@yahoo.com>
2024-05-22 12:05:13 +0530

    Update youtube harvesting.py

--- youtube harvesting.py
+++ youtube harvesting.py
@@ -6,7 +6,7 @@ import datetime
 
 # API key connection
 def Api_connect():
-    Api_Id = "AIzaSyBQUHaUibSzgfoe1gAfzLzvqJkQIgctIgY"
+    Api_Id = "-----------"
     api_service_name = "youtube"
     api_version = "v3"
     youtube = build(api_service_name, api_version, developerKey=Api_Id)
